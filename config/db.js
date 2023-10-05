baacae788231a71b795cc6546164f462e71a5349
Fazi7845 <125380301+Fazi7845@users.noreply.github.com>
2023-10-05 02:43:59 -0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToMongoDB = async () => {
   try {
-    const dbUrl = "mongodb+srv://faizanrauf797979:8SN6PHh1w4czSWOk@cluster0.ljedzmb.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp"; // Replace with your MongoDB connection URL
+    const dbUrl = ""; // Replace with your MongoDB connection URL
     await mongoose.connect(dbUrl, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
